3682790c1ca683c9b31d0650b47efe06d54c063e
Kassi <70130263+kassi-correia@users.noreply.github.com>
2022-04-27 14:45:56 -0400

    Update run in santoriniCLI

--- SantoriniCLI.py
+++ SantoriniCLI.py
@@ -1,12 +1,16 @@
+import sys
+from Exception import InvalidMove, InvalidBuild, InvalidWorker
+
 
 class SantoriniCLI():
-    
+
     def __init__(self):
         pass
     
     def _display_menu(self):
         pass
     
-    def run(self):
+    def run(self, white='human', blue='human', undo='off', score='off'):      
+        """Initialize game"""
         pass
-        
+
