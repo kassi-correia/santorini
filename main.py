d86d2abecfb10a3889bcaa18dcd292aa29bf48a6
Kassi <70130263+kassi-correia@users.noreply.github.com>
2022-04-27 14:46:46 -0400

    Command line arg checking main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import sys
+from SantoriniCLI import SantoriniCLI
+from Exception import InvalidArgs
 
 
 
@@ -6,4 +9,45 @@
 
 
 if __name__ == "__main__":
-    print("running...")
+    try:
+        print("running...")
+        if (len(sys.argv)) > 5 or (len(sys.argv)) < 1:
+            raise InvalidArgs()
+
+        elif (len(sys.argv)) == 1:
+            SantoriniCLI().run()
+
+        elif (len(sys.argv)) == 2:
+            if (sys.argv[1]) not in  ['human', 'heuristic', 'random']:
+                raise InvalidArgs()
+            SantoriniCLI().run(sys.argv[1])
+
+        elif (len(sys.argv)) == 3:
+            if (sys.argv[1]) not in  ['human', 'heuristic', 'random']:
+                raise InvalidArgs()
+            if (sys.argv[2]) not in  ['human', 'heuristic', 'random']:
+                raise InvalidArgs()
+            SantoriniCLI().run(sys.argv[1], sys.argv[2])
+
+        elif (len(sys.argv)) == 4:
+            if (sys.argv[1]) not in  ['human', 'heuristic', 'random']:
+                raise InvalidArgs()
+            if (sys.argv[2]) not in  ['human', 'heuristic', 'random']:
+                raise InvalidArgs()
+            if (sys.argv[3]) not in  ['on', 'off']:
+                raise InvalidArgs()
+            SantoriniCLI().run(sys.argv[1], sys.argv[2], sys.argv[3])
+        
+        else:
+            if (sys.argv[1]) not in  ['human', 'heuristic', 'random']:
+                raise InvalidArgs()
+            if (sys.argv[2]) not in  ['human', 'heuristic', 'random']:
+                raise InvalidArgs()
+            if (sys.argv[3]) not in  ['on', 'off']:
+                raise InvalidArgs()
+            if (sys.argv[4]) not in  ['on', 'off']:
+                raise InvalidArgs()
+            SantoriniCLI().run(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+   
+    except InvalidArgs:
+        print("Invalid arguments")
