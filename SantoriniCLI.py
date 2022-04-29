98bceb99f94d285b97fd1c92eb057839a5ea9c3f
Robert Takizawa <robert.takizawa@yale.edu>
2022-04-28 23:40:36 -0400

    fixed main

--- SantoriniCLI.py
+++ SantoriniCLI.py
@@ -32,15 +32,15 @@ class SantoriniCLI():
         board = Game.git_board()
         printf("""
 +--+--+--+--+--+
-|{board[0][0]}|{board[0][1]}|{board[0][2]}|{board[0][3]}|{board[0][4]}|
+|{board[0][0][0]}{board[0][0][1]}|{board[0][1][0]}{board[0][1][1]}|{board[0][2][0]}{board[0][2][1]}|{board[0][3][0]}{board[0][3][1]}|{board[0][4][0]}{board[0][4][1]}|
 +--+--+--+--+--+
-|{board[1][0]}|{board[1][1]}|{board[1][2]}|{board[1][3]}|{board[1][4]}|
+|{board[1][0][0]}{board[1][0][1]}|{board[1][1][0]}{board[1][1][1]}|{board[1][2][0]}{board[1][2][1]}|{board[1][3][0]}{board[1][3][1]}|{board[1][4][0]}{board[1][4][1]}|
 +--+--+--+--+--+
-|{board[2][0]}|{board[2][1]}|{board[2][2]}|{board[2][3]}|{board[2][4]}|
+|{board[2][0][0]}{board[2][0][1]}|{board[2][1][0]}{board[2][1][1]}|{board[2][2][0]}{board[2][2][1]}|{board[2][3][0]}{board[2][3][1]}|{board[2][4][0]}{board[2][4][1]}|
 +--+--+--+--+--+
-|{board[3][0]}|{board[3][1]}|{board[3][2]}|{board[3][3]}|{board[3][4]}|
+|{board[3][0][0]}{board[3][0][1]}|{board[3][1][0]}{board[3][1][1]}|{board[3][2][0]}{board[3][2][1]}|{board[3][3][0]}{board[3][3][1]}|{board[3][4][0]}{board[3][4][1]}|
 +--+--+--+--+--+
-|{board[4][0]}|{board[4][1]}|{board[4][2]}|{board[4][3]}|{board[4][4]}|
+|{board[4][0][0]}{board[4][0][1]}|{board[4][1][0]}{board[4][1][1]}|{board[4][2][0]}{board[4][2][1]}|{board[4][3][0]}{board[4][3][1]}|{board[4][4][0]}{board[4][4][1]}|
 +--+--+--+--+--+
         """)
         pass
