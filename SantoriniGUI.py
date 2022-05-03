cb68ffd2a89e7d9c8bf9630e9b99cb65d0d4fcc5
Kassi Correia <kassicorreia@Kassis-MBP.wireless.yale.internal>
2022-05-03 14:48:55 -0400

    lol

--- SantoriniGUI.py
+++ SantoriniGUI.py
@@ -1,1 +1,7 @@
 import tkinter as tk
+
+
+class BankGUI():
+    def __init__(self):
+        self._window = tk.Tk()
+        self._window.title("MY BANK")
---
+++ gui_main.py
@@ -0,0 +1,52 @@
+import sys
+from SantoriniGUI import SantoriniGUI
+from Exception import InvalidArgs
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    try:
+        if (len(sys.argv)) > 5 or (len(sys.argv)) < 1:
+            raise InvalidArgs()
+
+        elif (len(sys.argv)) == 1:
+            SantoriniGUI().run()
+
+        elif (len(sys.argv)) == 2:
+            if (sys.argv[1]) not in  ['human', 'heuristic', 'random']:
+                raise InvalidArgs()
+            SantoriniGUI(sys.argv[1]).run()
+
+        elif (len(sys.argv)) == 3:
+            if (sys.argv[1]) not in  ['human', 'heuristic', 'random']:
+                raise InvalidArgs()
+            if (sys.argv[2]) not in  ['human', 'heuristic', 'random']:
+                raise InvalidArgs()
+            SantoriniGUI(sys.argv[1], sys.argv[2]).run()
+
+        elif (len(sys.argv)) == 4:
+            if (sys.argv[1]) not in  ['human', 'heuristic', 'random']:
+                raise InvalidArgs()
+            if (sys.argv[2]) not in  ['human', 'heuristic', 'random']:
+                raise InvalidArgs()
+            if (sys.argv[3]) not in  ['on', 'off']:
+                raise InvalidArgs()
+            SantoriniGUI(sys.argv[1], sys.argv[2], sys.argv[3]).run()
+        
+        else:
+            if (sys.argv[1]) not in  ['human', 'heuristic', 'random']:
+                raise InvalidArgs()
+            if (sys.argv[2]) not in  ['human', 'heuristic', 'random']:
+                raise InvalidArgs()
+            if (sys.argv[3]) not in  ['on', 'off']:
+                raise InvalidArgs()
+            if (sys.argv[4]) not in  ['on', 'off']:
+                raise InvalidArgs()
+            SantoriniGUI(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]).run()
+   
+    except InvalidArgs:
+        print("Invalid arguments")
